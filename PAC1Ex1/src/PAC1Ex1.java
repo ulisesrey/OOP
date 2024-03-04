4040d1a8e9d3c9a26cb505c532e7c49739cde938
Ulises Rey <ulises.rey@univie.ac.at>
2024-03-04 16:40:05 +0100

    first commit

---
+++ PAC1Ex1/src/PAC1Ex1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class PAC1Ex1 {
+
+    private static double motzkin(int n, double motzkinNMinus1, double motzkinNMinus2) {
+        // Base cases
+        if (n == 0 || n == 1) return 1;
+        // Other cases
+        // double my_result  = ((2.0 * n + 1) * motzkinNMinus1) / (n + 2) + ((3.0 * n - 3) * motzkinNMinus2) / (n + 2);
+        return ((2.0 * n + 1) * motzkinNMinus1) / (n + 2) + ((3.0 * n - 3) * motzkinNMinus2) / (n + 2);
+    }
+
+    public static void printMotzkin(int n) {
+        double motzkin, motzkinNMinus1 = 1, motzkinNMinus2 = 1;
+
+        for (int i = 0; i <= n; i++) {
+
+            motzkin = motzkin(i, motzkinNMinus1, motzkinNMinus2);
+            System.out.print(motzkin + ", ");
+
+            motzkinNMinus2 = motzkinNMinus1;
+            motzkinNMinus1 = motzkin;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        do {
+            System.out.print("Enter the value of N (only whole numbers): ");
+            n = scanner.nextInt();
+
+            if (n <= 0) {
+                System.out.print("The value of N must be a whole number. Try again.");
+            }
+        } while (n <= 0);
+
+        printMotzkin(n);
+    }
+}
---
+++ PAC1Ex2/src/edu/uoc/pac1/PAC1Ex2.java
@@ -0,0 +1,30 @@
+package edu.uoc.pac1;
+
+public class PAC1Ex2 {
+
+    private static final double LEVEL_MULTIPLIER = 4.5;
+    private static final double[] DIFFICULTY_MULTIPLIER = {1.0, 1.5, 2.5, 5.0};
+    private static final int GOLD_BASE = 100;
+    public static String ERR_LEVEL = "[ERROR] Invalid level";
+
+    public static boolean isValidLevel(byte level) {
+        //TODO
+    }
+
+    public static boolean isValidDifficulty(byte difficulty) {
+        //TODO
+    }
+
+    public static double getMultiplierByDifficulty(byte difficulty) {
+        //TODO
+    }
+
+    public static double calculateGold(byte level, byte difficulty) {
+        //TODO
+    }
+
+    public static void getAchievementsGold(byte level, byte[] difficulties) {
+        //TODO
+    }
+
+}
