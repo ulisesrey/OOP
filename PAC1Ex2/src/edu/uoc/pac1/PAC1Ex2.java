20e25fc772ea24945dd06c874450d44b0ceaed5a
Ulises Rey <ulises.rey@univie.ac.at>
2024-03-04 17:29:15 +0100

    added missing functions

--- PAC1Ex2/src/edu/uoc/pac1/PAC1Ex2.java
+++ PAC1Ex2/src/edu/uoc/pac1/PAC1Ex2.java
@@ -8,23 +8,40 @@ public class PAC1Ex2 {
     public static String ERR_LEVEL = "[ERROR] Invalid level";
 
     public static boolean isValidLevel(byte level) {
-        //TODO
+        return level > 0 && level < 100;
     }
 
     public static boolean isValidDifficulty(byte difficulty) {
-        //TODO
+        return difficulty >= 0 && difficulty <= 4;
     }
 
+
     public static double getMultiplierByDifficulty(byte difficulty) {
-        //TODO
+        // Multiply difficulty variable by the corresponding variable in DIFICULTY_MULTIPLIER array
+        return DIFFICULTY_MULTIPLIER[difficulty];
     }
 
     public static double calculateGold(byte level, byte difficulty) {
-        //TODO
+        // Add Goldbase and multiply it by the level and the difficulty multiplier
+        return (GOLD_BASE + (level * LEVEL_MULTIPLIER)) * getMultiplierByDifficulty(difficulty);
     }
 
     public static void getAchievementsGold(byte level, byte[] difficulties) {
-        //TODO
+        // Return the sum of the gold for each difficulty
+        double totalGold = 0;
+        int invalidDifficulties = 0;
+
+        for (byte difficulty : difficulties) {
+            if (isValidDifficulty(difficulty)) {
+                totalGold += calculateGold(level, difficulty);
+            } else {
+                invalidDifficulties++;
+            }
+        }
+        // print the sum of the gold and the number of invalid difficulties
+        System.out.println("You have obtained a total of " + totalGold + " gold from " + difficulties.length + " achievements.");
+        // print the number of invalid difficulties
+        System.out.println(invalidDifficulties + " achievements with invalid difficulties have been detected.");
     }
 
 }
