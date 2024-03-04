21b72089930f989374dfc1924e01d7b80b3dc48b
Ulises Rey <ulises.rey@univie.ac.at>
2024-03-04 17:10:40 +0100

    working test unit

--- PAC1Task3/test/PAC1Task3Test.java
+++ PAC1Task3/test/PAC1Task3Test.java
@@ -1,7 +1,9 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PAC1Task3Test {
     @Test
     public void testProduct() {
-        assertEquals(42, PAC1Task3.product(6, 7));
+        assertEquals(44, PAC1Task3.product(6, 7));
     }
 }
