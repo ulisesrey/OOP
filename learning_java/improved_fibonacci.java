69f888a7d77a9a0948b20b916e182d75cd55abe2
Ulises Rey <ulises.rey@univie.ac.at>
2024-03-11 17:45:34 +0100

    added fibbonaci exercise from book

---
+++ learning_java/improved_fibonacci.java
@@ -0,0 +1,22 @@
+class improved_fibonacci {
+    static final int MAX_INDEX = 100;
+    /*
+     * Print out the first few Fibonacci numbers.
+     * Mark the evens with a *
+     */
+    public static void main(String[] args){
+        int lo = 1;
+        int hi =1;
+        String mark;
+        System.out.println("1: " + lo);
+        for (int i = 2; i <= MAX_INDEX; i++) {
+            if (hi % 2 ==0)
+                mark = " *";
+            else
+                mark = "";
+            System.out.println(i + ": " + hi + mark);
+            hi = lo + hi;
+            lo = hi - lo;
+        }
+    }
+}
