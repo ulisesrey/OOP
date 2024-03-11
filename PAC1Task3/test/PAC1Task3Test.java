270d446ed37ba3ed7b24dfb4c25738caa77afc4d
Ulises Rey <ulises.rey@univie.ac.at>
2024-03-11 18:38:30 +0100

    fixed it back to expected value

--- PAC1Task3/test/PAC1Task3Test.java
+++ PAC1Task3/test/PAC1Task3Test.java
@@ -4,6 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PAC1Task3Test {
     @Test
     public void testProduct() {
-        assertEquals(44, PAC1Task3.product(6, 7));
+        assertEquals(42, PAC1Task3.product(6, 7));
     }
 }
