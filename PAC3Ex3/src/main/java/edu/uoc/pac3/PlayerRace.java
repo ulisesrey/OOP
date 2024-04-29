9b6c3536b0154164e05993360533ec96e500c97b
Ulises Rey <ulises.rey@univie.ac.at>
2024-04-29 12:32:11 +0200

    first version of the PlayerRace class. Throws lots of errors

---
+++ PAC3Ex3/src/main/java/edu/uoc/pac3/PlayerRace.java
@@ -0,0 +1,71 @@
+package edu.uoc.pac3;
+
+public enum PlayerRace {
+    WARRIOR("Warrior", 150, 18, 6, 20, 10),
+    NINJA("Ninja", 120, 12, 14, 15, 18),
+    SHAMAN("Shaman", 110, 10, 20, 10, 12),
+    DARK_MAGE("DarkMage", 130, 15, 15, 18, 10);
+    private final String name;
+    private final int maxHP;
+    private final int vitality;
+    private final int intelligence;
+    private final int strength;
+    private final int agility;
+
+    PlayerRace(String name, int maxHP, int vitality, int intelligence, int strength, int agility) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.vitality = vitality;
+        this.intelligence = intelligence;
+        this.strength = strength;
+        this.agility = agility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public static PlayerRace getRace(String name) {
+        for (PlayerRace race : PlayerRace.values()) {
+            if (race.getName().equalsIgnoreCase(name)) {
+                return race;
+            }
+        }
+        return null;
+    }
+
+    public PlayerRace getBestPartner() {
+        switch (this) {
+            case WARRIOR:
+                return SHAMAN;
+            case SHAMAN:
+                return WARRIOR;
+            case NINJA:
+                return DARK_MAGE;
+            case DARK_MAGE:
+                return NINJA;
+            default:
+                return null;
+        }
+    }
+}
