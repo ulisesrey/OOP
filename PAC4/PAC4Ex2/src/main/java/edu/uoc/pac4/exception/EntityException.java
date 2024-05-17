3a324e31a74a1f9914bbba9e6cf9451f8cc84133
Ulises Rey <ulises.rey@univie.ac.at>
2024-05-17 12:30:16 +0200

    fixed variable name

--- PAC4/PAC4Ex2/src/main/java/edu/uoc/pac4/exception/EntityException.java
+++ PAC4/PAC4Ex2/src/main/java/edu/uoc/pac4/exception/EntityException.java
@@ -3,7 +3,7 @@ package edu.uoc.pac4.exception;
 public class EntityException extends Exception {
     public static final String INVALID_NAME = "Name cannot be null or empty";
     public static final String INVALID_LEVEL = "Level must be between 1 and 99";
-    public static final String INVALID_POSITION = "Position cannot be null";
+    public static final String POSITION_NULL = "Position cannot be null"; // Add this line
 
     public EntityException(String message) {
         super(message);
