357b4fb939fcae375e142267ef4dc6722431b380
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-21 17:47:34 +0200

    readded wagon

--- OOP_PR/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Seat.java
+++ OOP_PR/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Seat.java
@@ -4,11 +4,13 @@ public class Seat {
     private int number;
     private SeatType seatType;
     private boolean available;
+    private String wagonId;
 
-    public Seat(int number, SeatType seatType, boolean available) {
+    public Seat(int number, SeatType seatType, boolean available, String wagonId) {
         this.number = number;
         this.seatType = seatType;
         this.available = available;
+        this.wagonId = wagonId;
     }
 
     public int getSeatNumber() {
@@ -16,7 +18,7 @@ public class Seat {
     }
 
     public String getWagonId() {
-        return this.seat.getWagonId();
+        return this.wagonId;
     }
 
     public SeatType getType() {
