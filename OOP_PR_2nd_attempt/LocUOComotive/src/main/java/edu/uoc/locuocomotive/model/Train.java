494e24d66047f5ff84a839356016a5d9d41f94f9
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-23 10:59:48 +0200

    added constructor

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Train.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Train.java
@@ -3,5 +3,11 @@ package edu.uoc.locuocomotive.model;
 public class Train {
     public int id;
     public String model;
-    public String[] cars;
+    public int[] cars;
+
+    public Train(int id, String model, int[] cars) {
+        this.id = id;
+        this.model = model;
+        this.cars = cars;
+    }
 }
