bcc39bc161a23bc7fabf6d2e1b0dbd5f049357ab
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-23 10:59:02 +0200

    added methods

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Route.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Route.java
@@ -1,13 +1,24 @@
 package edu.uoc.locuocomotive.model;
 
 public class Route {
-    public int id;
+    public String id;
     public int trainId;
     public String[] StationsAndTimes;
 
-    public Route(int id, int trainId, String[] StationsAndTimes) {
+    public Route(String id, int trainId, String[] StationsAndTimes) {
         this.id = id;
         this.trainId = trainId;
         this.StationsAndTimes = StationsAndTimes;
+    }
+
+    public int getDepartureStationId() {
+        int stationId = Integer.parseInt(StationsAndTimes[0].split("|")[0]);
+        return stationId;
+    }
+    @Override
+    public String toString() {
+        return id + "|" +
+        trainId + "|" +
+        String.join(",", StationsAndTimes);
     }
 }
