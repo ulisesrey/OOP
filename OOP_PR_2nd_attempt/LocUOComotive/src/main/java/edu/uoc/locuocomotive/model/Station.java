2dc7582d66f893eb798e8976347d331fb1f3e109
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-23 10:59:33 +0200

    added setters, getters and toString() getters and setters probably not needed

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Station.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Station.java
@@ -20,4 +20,64 @@ public class Station {
         this.positionX = positionX;
         this.positionY = positionY;
     }
+    public int setId(int id) {
+        return this.id = id;
+    }
+    public String setName(String name) {
+        return this.name = name;
+    }
+    public String setCity(String city) {
+        return this.city = city;
+    }
+    public int setOpeningYear(int openingYear) {
+        return this.openingYear = openingYear;
+    }
+    public String setType(String type) {
+        return this.type = type;
+    }
+    public String setImage(String image) {
+        return this.image = image;
+    }
+    public int setPositionX(int positionX) {
+        return this.positionX = positionX;
+    }
+    public int setPositionY(int positionY) {
+        return this.positionY = positionY;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getCity() {
+        return this.city;
+    }
+    public int getOpeningYear() {
+        return this.openingYear;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public String getImage() {
+        return this.image;
+    }
+    public int getPositionX() {
+        return this.positionX;
+    }
+    public int getPositionY() {
+        return this.positionY;
+    }
+
+    @Override
+    public String toString() {
+        return id + "|" +
+                name + "|" +
+                city + "|" +
+//                openingYear + "|" +
+//                type + "|" +
+                image + "|" +
+                positionX + "|" +
+                positionY;
+    }
 }
