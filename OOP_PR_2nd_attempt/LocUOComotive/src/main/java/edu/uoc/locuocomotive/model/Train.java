e7aa7e87a5e14a5701f32fb19dbc4b4f65dc034a
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-23 19:38:03 +0200

    Hoping to create the wagon objects during the Train constructor

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Train.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Train.java
@@ -1,14 +1,29 @@
 package edu.uoc.locuocomotive.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Train {
     public int id;
     public String model;
     public int[] cars;
+    public List<Wagon> wagons; // = new ArrayList<Wagon>();
 
     public Train(int id, String model, int[] cars) {
         this.id = id;
         this.model = model;
         this.cars = cars;
+        this.wagons = new ArrayList<>();
+        createWagons();
+    }
+
+    private void createWagons() {
+        for (int i = 0; i < cars.length; i++) {
+            // Assuming each car in the array corresponds to a wagon with the same index
+            // The type of wagon can be determined based on your criteria, here using "Passenger" as default
+            Wagon wagon = new Wagon(i + 1, this, cars[i]);
+            wagons.add(wagon);
+        }
     }
 
     public Train getTrainById(int id) {
@@ -23,7 +38,7 @@ public class Train {
         return model;
     }
 
-    public int[] getCars() {
+    public int[] getWagons() {
         return cars;
     }
     @Override
