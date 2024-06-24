58945ddf2aa4df45ce142fb77f88c241e346e2b2
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-24 14:18:11 +0200

    changed public to private Passenger classes

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Passenger.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Passenger.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Passenger {
     private String passport;
-    public String name;
-    public String surName;
-    public LocalDate birthDate;
-    public String email;
+    private String name;
+    private String surName;
+    private LocalDate birthDate;
+    private String email;
 
     public Passenger(String passport, String name, String surName, LocalDate birthDate, String email) {
         this.passport = passport;
@@ -18,17 +18,34 @@ public class Passenger {
         this.email = email;
     }
 
+    public String getPassport() {
+        return passport;
+    }
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
     }
     public void setSurName(String surName) {
         this.surName = surName;
     }
 
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
 
+    public String getEmail() {
+        return email;
+    }
     public void setEmail(String email) {
         // THis should check for email string to be proper.
         this.email = email;
@@ -39,22 +56,5 @@ public class Passenger {
     public String toString() {
         return passport + "|" + name  + "|" + surName + "|" + birthDate +"|"+ email;
     }
-
-//    public String getPassport() {
-//        return passport;
-//    }
-//
-//    // Write the setters
-//    public String setName() {
-//        this.name = name;}
-//    public String setSurname() {
-//        this.surName = surName;}
-//    }
-//    public LocalDate setBirthDate() {
-//        this.birthDate = birthDate;}
-//    }
-//    public String setEmail() {
-//        this.email = email;}
-//    }
 
 }
