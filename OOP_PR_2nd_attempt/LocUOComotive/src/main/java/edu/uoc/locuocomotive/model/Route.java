13a0221266f159068f6b5994164d1c1c5b9baefa
Ulises Rey <ulises.rey@univie.ac.at>
2024-06-24 14:14:52 +0200

    improved Route class

--- OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Route.java
+++ OOP_PR_2nd_attempt/LocUOComotive/src/main/java/edu/uoc/locuocomotive/model/Route.java
@@ -1,9 +1,9 @@
 package edu.uoc.locuocomotive.model;
 
 public class Route {
-    public String id;
-    public int trainId;
-    public String[] StationsAndTimes;
+    private String id;
+    private int trainId;
+    private String[] StationsAndTimes;
 
     public Route(String id, int trainId, String[] StationsAndTimes) {
         this.id = id;
@@ -21,24 +21,14 @@ public class Route {
     }
 
     public String[] getStationsAndTimes() {
+
         return StationsAndTimes;
-    }
-    public int[] getDepartureStationIds(){
-        int[] stationIds = new int[StationsAndTimes.length];
-        for (int i = 0; i < StationsAndTimes.length; i++) {
-            stationIds[i] = Integer.parseInt(StationsAndTimes[i].split("|")[0]);
-        }
-        return stationIds;
     }
     public int getFirstDepartureStationId() {
         int stationId = Integer.parseInt(StationsAndTimes[0].split("|")[0]);
         return stationId;
     }
 
-    public int getLastDestinationStationId() { // Not sure this works.
-        int stationId =Integer.parseInt(StationsAndTimes[0].split("|")[-1]);
-        return stationId;
-    }
     @Override
     public String toString() {
         return id + "|" +
